5720ee6f3639990442ccc0ad9f72133364f4c51f
Blue31580 <kirkbailon31580@gmail.com>
2026-09-16 12:51:55 +0800

    Initial commit

---
+++ tools/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Appointment(models.Model):
+    #patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    #doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField(auto_now_add=True)
+    appointment_time = models.TimeField(auto_now_add=True)
+    appointment_status = models.CharField(max_length=20, default='pending')
+    appointment_details = models.TextField()
+
+    def __str__(self):
+        return self.appointment_details
